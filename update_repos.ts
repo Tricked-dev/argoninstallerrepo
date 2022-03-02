059e8b6548e1b252b8e3a5f12db0c690d46643c6
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2022-03-02 14:58:06 +0100

    feat: fix issues

--- update_repos.ts
+++ update_repos.ts
@@ -1,4 +1,6 @@
 import { feather, skyclient, std } from './src/mod.ts';
-
+try {
+	await Deno.mkdir('hashes');
+} catch (_) {}
 await Promise.all([feather(), skyclient(), std()]);
 await Deno.run({ cmd: ['deno', 'fmt', 'repos/'] }).status();
